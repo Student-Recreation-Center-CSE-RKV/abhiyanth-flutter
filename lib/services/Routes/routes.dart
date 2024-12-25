97830b5f9f312b60f13526a84eecb8867c119fd0
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-12-25 10:05:58 +0530

    Added icons, text, background widgets

--- lib/services/Routes/routes.dart
+++ lib/services/Routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:abhiyanth/views/signup_view.dart';
 import 'package:abhiyanth/views/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:abhiyanth/views/home_page.dart';
 
 class Routes{
   static Route<dynamic> generateRoute(RouteSettings settings){
@@ -14,6 +15,9 @@ class Routes{
         return MaterialPageRoute(builder: (BuildContext context)=> SignUpPage());
       case RoutesName.splash:
         return MaterialPageRoute(builder: (BuildContext context)=>SplashScreen());
+      case RoutesName.home:
+        return MaterialPageRoute(builder: (BuildContext context)=>const HomePage());
+      
       default:
         return MaterialPageRoute(builder: (_){
           return Scaffold(
