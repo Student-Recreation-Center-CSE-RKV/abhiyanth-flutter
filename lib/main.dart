eb47fb356ef3c94b4b019aabfcf49be0c016e149
sayyad-asifbasha <asifbasha4873@gmail.com>
2025-01-24 00:06:44 +0530

    add payments

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:abhiyanth/locator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+Future<void> firebaseBackgroundHandler(RemoteMessage message) async {
+  final Map<String, String?> payload = message.data.map((key, value) => MapEntry(key, value.toString()));
 
+  await AwesomeNotifications().createNotification(
+    content: NotificationContent(
+      id: -1,
+      channelKey: 'abhiyanth_channel',
+      title: message.data['title'] ?? 'No Title',
+      body: message.data['body'] ?? 'No Body',
+      summary: message.data['summary'],
+      bigPicture: message.data['bigPicture'],
+      notificationLayout: message.data['bigPicture'] != null
+          ? NotificationLayout.BigPicture
+          : NotificationLayout.BigText,
+      payload: payload,
+    ),
+  );
+  notificationServices.showNotification(message);
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
   setUpLocator();
-  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseBackgroundHandler);
+  notificationServices.initializeNotification();
+  notificationServices.getToken();
   runApp(
     ProviderScope(
       child: MyApp(),
